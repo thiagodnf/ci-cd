9d374e08d3a5a0b22e0963df30bc51e8785944d0
Thiago Ferreira <thiagodnf@users.noreply.github.com>
2026-03-04 19:05:27 -0500

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,9 +1,9 @@
 export function validate(nickname) {
 
     const rules = {
-        length: nickname.length >= 4 && nickname.length <= 20,
+        length: nickname.length >= 3 && nickname.length <= 20,
         startsWithLetter: /^[A-Za-z]/.test(nickname),
-        allowedChars: /^[A-Za-z0-9]+$/.test(nickname),
+        allowedChars: /^[A-Za-z0-9_]+$/.test(nickname),
         noConsecutiveUnderscores: !/__/.test(nickname),
         notEndWithUnderscore: !/_$/.test(nickname)
     };
